5f235be3d3c98b2cbfb8daa07ad95511529ef8fe
bmtechua <bmtech@i.ua>
2022-03-20 15:44:54 +0200

    probe1

---
+++ 19_TODO_2/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  19_TODO_2
+//
+//  Created by bmtech on 19.03.2022.
+//
+
+import Foundation
+
+struct Data {
+    var name: String
+    var desc: String
+}
--- 19_TODO_2/ViewController.swift
+++ 19_TODO_2/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTable: UITableView!
+    var identifier = "cell"
+    var arr: [Data] = []
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createTable()
+        
+        
     }
-
-
+    func createTable () {
+        myTable.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        self.myTable.delegate = self
+        self.myTable.dataSource = self
+        
+        view.addSubview(myTable)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? AddViewController else {
+            return
+        }
+        vc.delegate = self
+    }
+   
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        let item = arr[indexPath.row]
+       
+        var content = cell.defaultContentConfiguration()
+        content.text = "text"
+        content.secondaryText = "test"
+        cell.contentConfiguration = content
+        
+        
+        return cell
+    }
 }
 
+extension ViewController: AddName {
+    func appName(_ name: String, _ disc: String) {
+        nameLabel.text = name
+        descLabel.text = disc
+    }
+    
+    func appArray(_ name: String, _ disc: String) {
+        arr.append(Data(name: name, desc: disc))
+        
+        print(arr.count)
+    }
+    
+    
+}
